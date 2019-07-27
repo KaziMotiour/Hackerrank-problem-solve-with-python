d7caf607463f4134e7a5ade68c664f09089c8e22
Your Name <kmatiour30@gmail.com>
2019-07-27 13:53:19 +0600

    Second commit

--- The Minion Game.py
+++ The Minion Game.py
@@ -1,16 +1,32 @@
 String=input()
 Vowels=['A','E','I','O','U']
+Consonant=['B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z']
 Vow=[]
 cons=[]
 dic={}
 for i in range(len(String)):
     for j in range(len(Vowels)):
-        if String[i] == Vowels[j]:
+        if String[i] in Vowels[j]:
+           # print(Vowels[j])
             Vow.append(String[i:len(String)])
-        else:
+
+for i in range(len(String)):
+    for j in range(len(Consonant)):
+        if String[i] in Consonant[j]:
             cons.append(String[i:len(String)])
-Vowel=Vow[0]
-Consonant=cons[0]
+Vowel = Vow[0]
+consonant = cons[0]
 k=0
-v=0
-print(dic.items())
+p=0
+c=[]
+for i in range(len(consonant)):
+    for j in range(len(consonant)):
+        for k in range(len(Consonant)):
+
+            con = consonant[0:0]
+            print(consonant[0:0])
+            if con[0] in Consonant[k]:
+                c.append(con)
+
+
+print(c)
