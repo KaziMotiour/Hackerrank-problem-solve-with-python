c3c952d552ec010a257f440031645fcc4316c6d6
Your Name <kmatiour30@gmail.com>
2019-07-28 23:51:58 +0600

    first commit

--- The Minion Game.py
+++ The Minion Game.py
@@ -1,32 +1,14 @@
-String=input()
-Vowels=['A','E','I','O','U']
-Consonant=['B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z']
-Vow=[]
-cons=[]
-dic={}
+String = input()
+Vowels = ['A','E','I','O','U']
+vow = 0
+cons = 0
+length = len(String)
+dic = {}
 for i in range(len(String)):
-    for j in range(len(Vowels)):
-        if String[i] in Vowels[j]:
-           # print(Vowels[j])
-            Vow.append(String[i:len(String)])
+    if String[i] in Vowels:
+        vow = String[i:length] if vow == 0 else vow
+    else:
+        cons = String[i:length] if cons == 0 else vow
 
-for i in range(len(String)):
-    for j in range(len(Consonant)):
-        if String[i] in Consonant[j]:
-            cons.append(String[i:len(String)])
-Vowel = Vow[0]
-consonant = cons[0]
-k=0
-p=0
-c=[]
-for i in range(len(consonant)):
-    for j in range(len(consonant)):
-        for k in range(len(Consonant)):
-
-            con = consonant[0:0]
-            print(consonant[0:0])
-            if con[0] in Consonant[k]:
-                c.append(con)
-
-
-print(c)
+print(vow)
+print(cons)
