e7c62040be567771b6c03bb2c93ac40c20aafc32
Your Name <kmatiour30@gmail.com>
2019-08-19 21:55:47 +0600

    first commit

--- Marge the tools2.py
+++ Marge the tools2.py
@@ -1,19 +1,27 @@
+import math
 def merge_the_tools(string, k):
     def ops(y):
         y[0] += k
         y[1] += k
-        return y[1]
+        return 1
 
     y = [-k, 0]
-    subString = [string[y[0]:y[1]] for i in range(len(string)) if ops(y)<=len      (string)]
+    subString = [string[y[0]:y[1]] for i in range(math.ceil(len(string) / k)) if ops(y)]
     j = 0
     s = ''
+    m=0
     for i in range(len(subString)):
-        s += [i for i in subString[j] if (1 if i not in s else i)]
+       # s += [p for p in subString[j] if (1 if p in s else p)]
+        for i in subString[j]:
+            if subString[j].index(i)==m:
+                s+=i
+            m+=1
         j += 1
+        m=0
         print(s)
         s = ''
 
 if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
+
--- Merge the Tools!.py
+++ Merge the Tools!.py
@@ -1,46 +1,24 @@
-# import math
-# string,factor=input(),int(input())
-# subString=[]
-# j,k=0,factor
-# x=math.ceil(len(string)/factor)
-# for i in range(x):
-#        subString.append(string[j:k])
-#        j+=factor
-#        k+=factor
-# print(subString)
-
-# def opp(x):
-#     x[0]+=1
-#     x[1]+=1
-#     return x[1]
-# string,factor=input(),int(input())
-# subString=[]
-# q=len(string)
-# while q>factor:
-#        subString.append(string[q-factor:q])
-#        q-=factor
-# subString.append(string[0:q])
-# print(subString)
-# x=[0,1]
-# ti=[string[x[0]:x[1]] for i in range(len(string)) if opp(x)<=len(string)]
-# print(ti)
-
 import math
-string,factor=input(),int(input())
-def ops(y):
-    y[0]+=factor
-    y[1]+=factor
-    return 1
-y=[-factor,0]
-subString=[string[y[0]:y[1]] for i in range(math.ceil(len(string)/factor)) if ops(y)]
-#print(subString)
-j=0
-s=''
-for i in range(len(subString)):
-    for i in subString[j]:
-        if i not in s:
-            s += i
-    j+=1
-    print(s)
-    s=''
-#[print(i,end='') for i in final[0]]
+def merge_the_tools(string, k):
+    subString = []
+    j, l = 0, k
+    x = math.ceil(len(string) / k)
+    for i in range(x):
+        subString.append(string[j:l])
+        l += k
+        j += k
+    j = 0
+    s = ''
+    for i in range(len(subString)):
+        for k in subString[j]:
+            if k not in s:
+                s+=k
+
+        j += 1
+        print(s)
+        s = ''
+
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
