c29b00c0425e616ccbf15804a07cf2cd4a2bfc26
Your Name <kmatiour30@gmail.com>
2019-08-22 07:31:46 +0600

    first commit

--- itertools.product().py
+++ itertools.product().py
@@ -9,10 +9,12 @@
 # a = list(x)
 # b =[int(i) for i in list(input())]
 # print(b)
+
 # from itertools import product
 # a,b= [(int(i) for i in input().split()) for _ in range(2)]
-# [print(i, end='') for i in list(product(a, b))]
+# [print(i, end='') for i in product(a, b)]
 # print('\n')
 # print(*product(a, b))
-list=[1,2,3]
-print(*list)
+
+List = [1, 2, 3]
+print(*List)
